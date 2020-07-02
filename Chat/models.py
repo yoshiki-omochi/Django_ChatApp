0b77b7eb99974ba75d2a042608ca96863d8bbad0
omochi_suraimu <omochi.net.work@gmail.com>
2020-07-02 23:21:13 +0900

    add model.py file

--- Chat/models.py
+++ Chat/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(model.Model):
+    """投稿者のモデル"""
+
+    user = model.CharField(verbose_name = 'ユーザー名', max_length = 100, null = True, blank = True)
+    comment = model.TextField(verbose_name = 'コメント', blank = True, null = True, max_length = 1000,)
+    created_at = model.DateTimeField(verbose_name = '作成日時',auto_now_add = True)
+
+    class Meta:
+        verbose_name_plural = 'User'
+
+    def __str__(self):
+        return self.created_at
