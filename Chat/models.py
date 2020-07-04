7d9f7a2323034d443a10a2644ca9dfab3cd7e794
omochi_suraimu <omochi.net.work@gmail.com>
2020-07-04 15:08:40 +0900

    add forms file

--- Chat/models.py
+++ Chat/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 
-class User(model.Model):
+class User(models.Model):
     """投稿者のモデル"""
 
-    user = model.CharField(verbose_name = 'ユーザー名', max_length = 100, null = True, blank = True)
-    comment = model.TextField(verbose_name = 'コメント', blank = True, null = True, max_length = 1000,)
-    created_at = model.DateTimeField(verbose_name = '作成日時',auto_now_add = True)
+    name = models.CharField(verbose_name = 'ユーザー名', max_length = 100, null = True, blank = True)
+    comment = models.TextField(verbose_name = 'コメント', blank = True, null = True, max_length = 1000,)
+    created_at = models.DateTimeField(verbose_name = '作成日時',auto_now_add = True)
 
     class Meta:
         verbose_name_plural = 'User'
