5776b5b24b55a01a236d944826c3eee0acf73955
森本禎規 <yoshiki@Yoshiki-2.local>
2020-07-07 09:54:17 +0900

    add new file

--- Chat/urls.py
+++ Chat/urls.py
@@ -6,6 +6,6 @@ app_name = 'Chat'
 urlpatterns = [
     path('',views.IndexView.as_view(), name = 'index'),
     path('foo/',views.FooView.as_view(), name = 'foo'),
-    path('post/',views.post, name = 'post'),
+    path('post/',views.PostView.as_view(), name = 'post'),
     path('timeline/',views.TimelineView.as_view(), name = 'timeline')
 ]
--- Chat/views.py
+++ Chat/views.py
@@ -7,6 +7,8 @@ from django.views import generic
 from django.http import HttpResponse
 from Chat.forms import TestuserForm
 
+from django.views import View
+
 
 
 class IndexView(generic.TemplateView):
@@ -15,13 +17,20 @@ class IndexView(generic.TemplateView):
 class FooView(generic.TemplateView):
     template_name = "foo.html"
 
-class PostView(generic.TemplateView):
-    template_name = "post.html"
+class PostView(View):
+    
+    def get(self,request,*args,**kwargs):
+        f = TestuserForm()
+        return render(request, 'post.html', {'form1' : f})
+
+    def post(self, request, *args, **kwargs):
+        f = TestuserForm()
+        return render(request, 'post.html', {'form1' : f})
+        
+    
+
 
 class TimelineView(generic.TemplateView):
     template_name = "timeline.html"
 
 
-def post(request):
-    f = TestuserForm()
-    return render(request, 'post.html', {'form1' : f})
